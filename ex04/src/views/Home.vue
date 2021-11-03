5ef6a43ba68ec822041db5277fdd0e17d1dd6ae0
Petre Leonard Macamete <petreleonardos@gmail.com>
2021-11-03 16:24:52 +0200

    rotate and save

--- ex04/src/views/Home.vue
+++ ex04/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="inputoutput">
+    <img id="imageSrc" alt="No Image" v-on:load="onLoadImage" />
+    <div class="caption">imageSrc <input v-on:change='handleChange'
+      type="file" id="fileInput" name="file" /></div>
+  </div>
+  <div class="inputoutput">
+    <canvas id="canvasOutput" ></canvas>
+    <div class="caption">canvasOutput
+      <button v-on:click="handleRotate">Rotate</button>
+      <button v-on:click="handleSave">Save</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import cv from '@/assets/opencv';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    handleChange(e) {
+      const imgElement = document.getElementById('imageSrc');
+      imgElement.src = URL.createObjectURL(e.target.files[0]);
+    },
+    onLoadImage() {
+      const imgElement = document.getElementById('imageSrc');
+      const mat = cv.imread(imgElement);
+      cv.imshow('canvasOutput', mat);
+    },
+    handleRotate() {
+      const mat = cv.imread('canvasOutput');
+      cv.rotate(mat, mat, cv.ROTATE_90_CLOCKWISE);
+      cv.imshow('canvasOutput', mat);
+    },
+    handleSave() {
+      const link = document.createElement('a');
+      const canvas = document.getElementById('canvasOutput');
+      link.download = 'download.png';
+      link.href = canvas.toDataURL();
+      link.click();
+      link.delete();
+    },
   },
 };
 </script>
+
+<style>
+
+</style>
